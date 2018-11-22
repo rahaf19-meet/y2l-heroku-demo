b5ae71fd3840a95f6fdbd7153f781c22c6385b50
avigail18-meet <tutim2000@gmail.com>
2018-11-22 18:05:46 +0200

    g

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
+
 from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for
+from database import *
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
+def catbook_home():
+    cats = get_all_cats()
+    return render_template("home.html", cats=cats)
+
+@app.route('/cats/<int:id>')
+def cat_page(id):
+	cat = my_cat(id)
+	return render_template("cat.html", id=id , cat=cat)
+
+@app.route('/catnew', methods=["GET", "POST"])
+def new_cat():
+    if request.method == 'GET':
+        return render_template("new_cat.html")
+    else:
+    	name = request.form['firstname']
+        create_cat(name)
+        return redirect(url_for("catbook_home"))
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+   app.run(debug = True)
---
+++ database.py
@@ -0,0 +1,22 @@
+from models import *
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///cats.db')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def create_cat(name):
+    cat_object = Cat(name=name)
+    session.add(cat_object)
+    session.commit()
+
+def get_all_cats():
+    cats = session.query(Cat).all()
+    return cats
+
+def my_cat(id):
+	cat1= session.query(Cat).filter_by(id=id).first()
+	return cat1
